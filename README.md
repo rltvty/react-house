9cd6236b156c6fa219aaaa7974a8e4d3cbc3f6d3
Aaron LaBrie <aaron.labrie@returnpath.com>
2017-02-25 11:08:18 -0600

    CRA init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ERIC'S REACT HOUSE
+
+[CRA User Guide](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md)
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './app.css'
+
+const App = () => (
+  <div className='app'>
+    <h2>React House</h2>
+  </div>
+)
+
+export { App }
