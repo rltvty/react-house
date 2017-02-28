7db4f415af62d92e47e05ce2d32220c291070f20
Eric Pinzur <ericpinzur@otherinbox.com>
2017-02-28 05:26:50 -0600

    added routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import { LivingRoom } from './living_room'
+import { NavBar } from 'components/navbar'
+import { Home } from 'routes/home'
+import { LivingRoom } from 'routes/living_room'
+
 
 import 'normalize.css'
 import './index.css'
 
 const App = () => (
-  <div id='app'>
-    <LivingRoom />
-  </div>
+    <BrowserRouter>
+        <div className="app-wrapper">
+            <NavBar />
+            <div className="app">
+                <Route exact path="/" component={Home}/>
+                <Route path="/living" component={LivingRoom}/>
+            </div>
+        </div>
+    </BrowserRouter>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
